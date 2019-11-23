c9b0d2ce3059d86a04c3a61bc0f5c57b04f35480
will <ge35jum@mytum.de>
2019-11-23 18:01:18 +0100

    Barcode leser verbessert

--- detector.py
+++ detector.py
@@ -22,21 +22,35 @@ predictor = CustomVisionPredictionClient('7f4972bf8efe41db88c2e5c741d9309f',
                                          endpoint='https://hackatum2019.cognitiveservices.azure.com/')
 
 # Open the sample image and get back the prediction results.
-with open("C:/Users/Willi/Desktop/Food/test2.jpg", mode="rb") as test_data:
-    results = predictor.detect_image('0d5ff816-e2a8-4a81-bee7-cfcbe2293962', 'Iteration10', test_data)
-
+with open('C:/Users/Willi/Desktop/Food/test.jpg', mode="rb") as test_data:
+    try:
+        results = predictor.detect_image('0d5ff816-e2a8-4a81-bee7-cfcbe2293962', 'Iteration17', test_data)
+    except Exception as e:
+        print(e)
 # Display the results.
 Produkte = {}
+Barcode=""
 for prediction in results.predictions:
     if (prediction.probability > 0.4 ):
       if(prediction.tag_name== 'Studi'):
-          img = cv2.imread('C:/Users/Willi/Desktop/Food/test2.jpg')
+          img = cv2.imread('C:/Users/Willi/Desktop/Food/test.jpg')
           y=img.shape[0]
           x=img.shape[1]
-          crop_img = img[round(y*(prediction.bounding_box.top)):round(y*(prediction.bounding_box.top)+y*(prediction.bounding_box.height)),round( x*(prediction.bounding_box.left)):round(x*(prediction.bounding_box.left)+x*(prediction.bounding_box.width))]
+          crop_img = img[round(y*(prediction.bounding_box.top*0.9)):round(y*(prediction.bounding_box.top)+y*(prediction.bounding_box.height*1.1)),round( x*(prediction.bounding_box.left*0.9)):round(x*(prediction.bounding_box.left)+x*(prediction.bounding_box.width*1.1))]
+          (h, w) = crop_img.shape[:2]
+          center = (w / 2, h / 2)
+
+          i=1
           Barcode = decode(crop_img)
           if Barcode=="":
-              Barcode=-1
+            while i<=18:
+                M = cv2.getRotationMatrix2D(center,i*5, 0.8)
+                Barcode = decode(cv2.warpAffine(crop_img, M, (w, h)))
+
+                if Barcode!="":
+                     i=20
+                else:
+                     i+=1
       elif (prediction.tag_name in Produkte):
           Produkte[prediction.tag_name]+=1
       else:
