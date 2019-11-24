bfee75cc6cc8e23f52f49c52f4b65af7a53acedb
turing <julian@sikora--bremen.de>
2019-11-24 03:44:39 +0100

    add login

---
+++ app/templates/home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Gastro Pay</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="/assets/css/styles.min.css">
+</head>
+
+<body>
+    <?php require "components/navbar.php"; ?>
+
+    <div class="contact-clean" style="align:center">
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="card" style="margin: auto">
+            <div class="card-body">
+                <h2 class="text-center">Gastro Payment</h2>
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="/assets/img/" class="img-fluid" style="width:100%">
+                        <img src="/assets/img/" class="img-fluid" style="width:100%">
+                        <img src="/assets/img/" class="img-fluid" style="width:100%">
+                        <img src="/assets/img/" class="img-fluid" style="width:100%">
+                        <img src="/assets/img/" class="img-fluid" style="width:100%">
+                        <img src="/assets/img/" class="img-fluid" style="width:100%">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3"></div>
+    </div>
+</div>
+    <?php require "components/footer.php"; ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
