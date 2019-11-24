58c565bbe0039394e480a20cb08de8ba602d1b13
turing <julian@sikora--bremen.de>
2019-11-24 06:05:23 +0100

    navbar adjustment

--- app/templates/components/navbar.php
+++ app/templates/components/navbar.php
@@ -1,13 +1,12 @@
 <nav class="navbar navbar-light navbar-expand-md navigation-clean-button">
-        <div class="container"><a class="navbar-brand" href="/">Canteen Pay</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="container"><a class="navbar-brand" href="/">Gastro Pay</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse"
                 id="navcol-1">
                 <ul class="nav navbar-nav mr-auto">
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="/about">About</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="/transactions">My transactions</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="/admin">Admin</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="/login">Login</a></li>
                     <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Help</a>
-                        <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="/faq">FAQ</a><a class="dropdown-item" role="presentation" href="mailto:support@stei.ml">Email Support</a></div>
+                        <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="mailto:support@stei.ml">Email Support</a></div>
                     </li>
                 </ul>
             </div>
